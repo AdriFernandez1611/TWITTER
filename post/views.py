d3c061649449f4eb2c685238510efa25996103c3
adrifernandez1611 <adrifernandez1611@gmail.com>
2024-08-17 10:56:42 -0400

    pagina detail lista

--- post/views.py
+++ post/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+
 # Create your views here.
 
 class HomePageView(ListView):
 
     model= Post
     template_name= "home.html"
+    
+class DetailPageView(DetailView):
+    
+    model= Post
+    template_name= "detail.html"
