799bf7736b25dcf047379ab16ad6406eaa87465f
adrifernandez1611 <adrifernandez1611@gmail.com>
2024-08-17 09:24:30 -0400

    modelo y creacion

---
+++ post/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    titulo= models.CharField(max_length=40)
+    descripcion= models.TextField()
+    #imagen= models.ImageField(upload_to="media", blank=True, null=True)
+    autor= models.ForeignKey("auth.User", on_delete= models.CASCADE, blank=True)
+    
+    
